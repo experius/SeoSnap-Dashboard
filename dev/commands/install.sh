0b98c03d4531a0b1785ba75998426d920b47c16e
Egor Dmitriev <egordmitriev2@gmail.com>
2020-03-28 01:11:08 +0100

    [FEATURE] Added env file to docker ignore. Updated image name. Fixed secret generation script

--- dev/commands/install.sh
+++ dev/commands/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 if [ -f ".env" ]; then
   echo 'Generating new secret key'
-  export NEW_SECRET="$(base64 /dev/urandom | head -c50)";
+  export NEW_SECRET="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c50)";
   sed -i "s/https\:\/\/miniwebtool.com\/django-secret-key-generator\//${NEW_SECRET}/g" .env;
   
   echo 'Setting new admin user login'
   read -p 'Username [snaptron]: ' ADMIN_NAME
   read -p 'Email [snaptron@snap.tron]: ' ADMIN_EMAIL
   read -sp 'Password [Sn@ptron1337]: ' ADMIN_PASS
-  sed -i "s/snaptron/${ADMIN_NAME:-snaptron}/g" .env;
+  sed -i "s/snaptron$/${ADMIN_NAME:-snaptron}/g" .env;
   sed -i "s/snaptron@snap.tron/${ADMIN_EMAIL:-snaptron@snap.tron}/g" .env;
   sed -i "s/Sn@ptron1337/${ADMIN_PASS:-Sn@ptron1337}/g" .env;
 fi
